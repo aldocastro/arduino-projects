0c357d40a381ea2528a4a9f48b5ad45d3de4c1c5
aldobest <aldo.castro21@gmail.com>
2014-06-27 13:22:56 +0200

    adding usonic-tank-ic project

---
+++ usonic-tank-ic/src/sketch.ino
@@ -0,0 +1,75 @@
+
+/*******
+* Motor states
+* pin 2 and 7 in GND -> STOP
+* pin 2 and 7 in 5V  -> STOP
+* pin 2 in 5V  and pin 7 in GND -> DIRECTION A
+* pin 2 in GND and pin 7 in 5V  -> DIRECTION B
+*******/
+
+//	set the LC enable pin at 11
+int enablePin = 11;
+int enablePin2 = 3;
+//	pin 9 and 10 for motor_a
+int motor_a[] = {9, 10};
+//	pin 5 and 6 for motor_b
+int motor_b[] = {5, 6};
+
+//	set speeds
+int full_speed = 255;
+int middle_speed = 127;
+int cero_speed = 0;
+
+//	set delays
+int max_delay = 20000;
+int nor_delay = 10000;
+int min_delay = 5000;
+
+void setup() {
+	motorSetup();
+}
+ 
+void loop() {
+	forward(full_speed);
+	delay(max_delay);
+	stop();
+	delay(min_delay);
+	backward(full_speed);
+	delay(max_delay);
+	stop();
+}
+
+void stop() {
+	digitalWrite(motor_a[0], LOW);
+  	digitalWrite(motor_a[1], LOW);	
+
+  	digitalWrite(motor_b[0], LOW);
+  	digitalWrite(motor_b[1], LOW);
+}
+
+void forward(int speed) {
+	setMotor(motor_a, speed, HIGH);
+	setMotor(motor_b, speed, HIGH);
+}
+
+void backward(int speed) {
+	setMotor(motor_a, speed, LOW);
+	setMotor(motor_b, speed, LOW);	
+}
+
+
+void motorSetup () {
+	for(int i = 0; i < 2; i++){
+		pinMode(motor_a[i], OUTPUT);
+		pinMode(motor_b[i], OUTPUT);
+	}
+	pinMode(enablePin, OUTPUT);
+	pinMode(enablePin2, OUTPUT);
+} 
+
+void setMotor(int motor[], int speed, boolean reverse)
+{
+  analogWrite(enablePin, speed);
+  digitalWrite(motor[0], !reverse);
+  digitalWrite(motor[1], reverse);
+}
